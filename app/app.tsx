9ee829cb11d6dfc3603aecda6cb57f53837ddd17
dpasse <50343386+dpasse@users.noreply.github.com>
2023-06-10 09:48:49 -0500

    home page

--- app/app.tsx
+++ app/app.tsx
@@ -2,6 +2,8 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import { HomePage } from './home/HomePage';
+
 import './scss/styles.scss';
 
 const element = document.getElementById('app') as HTMLElement;
@@ -11,7 +13,7 @@ root.render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route index={true} element={<div>hi</div>} />
+        <Route index={true} element={<HomePage />} />
       </Routes>
     </Router>
   </StrictMode>
